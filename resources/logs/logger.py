b670a7cf80c2c9ec7c883a85bf8325a69af92b8a
wxmbugu <>
2024-05-14 13:17:35 +0300

    LOGGER: create subdirectories
    Signed-off-by: wxmbugu <swambugu@codefremics.com>

--- resources/logs/logger.py
+++ resources/logs/logger.py
@@ -4,35 +4,40 @@ from main import mysql, app
 from datetime import datetime, timedelta
 from resources.payload.payload import Localtime
 
-class ErrorLogger():
+
+class ErrorLogger:
     def logError(self, x):
-        x['datecreated'] = Localtime().gettime()
+        x["datecreated"] = Localtime().gettime()
         x = str(x)
-        file = os.path.join(app.config["UPLOAD_FOLDER"] + "/logs/Application_Erros.json")
-        with open(file, "w+") as fo:
+        file = os.path.join(
+            app.config["UPLOAD_FOLDER"] + "/logs/Application_Errors.json"
+        )
+        os.makedirs(os.path.dirname(file), exist_ok=True)
+        with open(file, "w") as fo:
             fo.write(x)
-        
+
         return Response({"Error logged successfully"}, status=200)
 
 
-
-class UssdLogger():
+class UssdLogger:
     def log(self, x):
-        x['datecreated'] = Localtime().gettime()
+        x["datecreated"] = Localtime().gettime()
         x = str(x)
         file = os.path.join(app.config["UPLOAD_FOLDER"] + "/logs/USSD_Requests.json")
-        with open(file, "w+") as fo:
+        os.makedirs(os.path.dirname(file), exist_ok=True)
+        with open(file, "w") as fo:
             fo.write(x)
-        
+
         return Response({"Request logged successfully"}, status=200)
-    
-class MpesaLogger():
+
+
+class MpesaLogger:
     def log(self, x):
-        x['datecreated'] = Localtime().gettime()
+        x["datecreated"] = Localtime().gettime()
         x = str(x)
         file = os.path.join(app.config["UPLOAD_FOLDER"] + "/logs/Mpesa_Logs.json")
-        with open(file, "w+") as fo:
+        os.makedirs(os.path.dirname(file), exist_ok=True)
+        with open(file, "w") as fo:
             fo.write(x)
-        
-        return Response({"Transaction logged successfully"}, status=200)
 
+        return Response({"Transaction logged successfully"}, status=200)
